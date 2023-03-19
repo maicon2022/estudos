13e5ca312bfc86cae5a006b00930d81599a7ecef
Maicon Felipe <maicon.felipe@academico.ifpb.edu.br>
2023-03-18 23:38:57 -0300

    atualização

--- python/maicon.py
+++ python/maicon.py
@@ -1,4 +1,26 @@
 print("Estudos Python sincronizados no GITHUB")
 
-def somar(a1,a2):
-    return a1 + a2
+'''Criação de arquivo texto através de python'''
+arquivo = open("python/maicon.txt","a")
+arquivo.write("Marcone,53,Masculino\n")
+arquivo.write("Maicon,21,Masculino\n")
+arquivo.write("Emily,13,Masculino\n")
+arquivo.close()
+
+# arquivo = open("python/maicon.txt","r")
+# linha = arquivo.readline()
+# while linha:
+#     print(linha)
+#     linha = arquivo.readline()
+# arquivo.close()
+
+# arquivo = open("python/maicon.txt","r")
+# linha = arquivo.read() #retorna numa única string
+# print(linha)
+# arquivo.close()
+
+arquivo = open("python/maicon.txt","r")
+for linha in arquivo:
+    campo = linha.split(',')
+    print('nome =',campo[0],'\tidade =',campo[1],'\tsexo',campo[2],end='')
+arquivo.close()    
